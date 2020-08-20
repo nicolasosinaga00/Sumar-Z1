85cabb1f3d058bab65be720ef90f621ba4c40018
nicolasosinaga00 <a2019114771@estudiantes.upsa.edu.bo>
2020-08-20 18:08:13 -0400

    Mi primer modelo de clase

---
+++ Sumar-Restar/Sumar/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sumar.Models;
+
+namespace Sumar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet]
+        public Person listar()
+        {
+            Person person = new Person()
+            {
+                PersonId = 1,
+                Name = "Nicolas Osinaga"
+            };
+            return person;
+        }
+    }
+}
